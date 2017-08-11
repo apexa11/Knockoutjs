0ab0c210835c535f2876e8e05ed2285daa02c0a1
apexa11 <apexachoxi7@gmail.com>
2017-08-11 10:03:32 -0400

    update

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var Viewmodel = function(){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("Abby");
 	this.imgSrc = ko.observable("img/cat.jpg");
+	this.Nickname = ko.observableArray(["nono,noni"]);
 
 	this.incrementCounter = function(){
 		this.clickCount(this.clickCount() + 1);
